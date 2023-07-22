1b6590dc4683134bb4319a7edae85e9e9389790b
kevinmc-teaching <35582572+kevinmc-teaching@users.noreply.github.com>
2023-07-22 16:24:15 -0700

    Initial commit.

---
+++ patterns/header.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Title: header
+ * Slug: wafflehouse//header
+ * Categories: hidden
+ * Inserter: no
+ */
+?>
+<!-- wp:cover {"url":"<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/image-00.webp","id":111,"dimRatio":70,"focalPoint":{"x":0.1,"y":0.9},"minHeight":400,"contentPosition":"bottom center","style":{"spacing":{"padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"},"margin":{"top":"0","bottom":"0"}}}} -->
+<div class="wp-block-cover has-custom-content-position is-position-bottom-center" style="margin-top:0;margin-bottom:0;padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px;min-height:400px"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-70 has-background-dim"></span><img class="wp-block-cover__image-background wp-image-111" alt="" src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/image-00.webp" style="object-position:10% 90%" data-object-fit="cover" data-object-position="10% 90%"/><div class="wp-block-cover__inner-container"><!-- wp:group {"layout":{"type":"default"}} -->
+<div class="wp-block-group"><!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:group -->
+<div class="wp-block-group"><!-- wp:site-title {"style":{"spacing":{"margin":{"bottom":"0px"}}}} /-->
+
+<!-- wp:site-tagline {"style":{"spacing":{"margin":{"top":"0","right":"0","bottom":"0","left":"0"}}}} /--></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:navigation {"ref":54,"overlayMenu":"never","layout":{"type":"flex","setCascadingProperties":true,"justifyContent":"center","orientation":"horizontal","flexWrap":"wrap"},"style":{"spacing":{"margin":{"top":"0"},"blockGap":"18px"}}} /--></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group --></div></div>
+<!-- /wp:cover -->
+
+<!-- wp:spacer {"height":"50px"} -->
+<div style="height:50px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
